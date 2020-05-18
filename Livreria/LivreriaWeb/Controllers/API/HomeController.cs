975166663af538c2cff40cf58be2324fee25e327
marcelovargas <marcelovargas.bo@gmail.com>
2020-05-18 17:14:57 -0300

    Autentificação JWT para API.

---
+++ Livreria/LivreriaWeb/Controllers/API/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LivreriaWeb.Models;
+using LivreriaWeb.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivreriaWeb.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("V1/Account")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = new User()
+            {
+                UserName = model.UserName,
+                Password = model.Password,
+            };
+
+            var token = TokenServices.GenerarateToken(model);
+            user.Password = string.Empty;
+            return new
+            {
+                user = user,
+                token = token,
+
+            };
+        }
+    }
+}
---
+++ Livreria/LivreriaWeb/Services/TokenServices.cs
@@ -0,0 +1,42 @@
+﻿using LivreriaWeb.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivreriaWeb.Services
+{
+    public static class TokenServices
+    {
+        public static string GenerarateToken (User user)
+        {
+            var tokenhandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+           
+            var tokenDescriptor = new SecurityTokenDescriptor
+            { 
+                Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.UserName.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(2),
+
+            SigningCredentials = new SigningCredentials(
+               new SymmetricSecurityKey(key),
+               SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenhandler.CreateToken(tokenDescriptor);
+
+            return tokenhandler.WriteToken(token);
+
+
+        }
+    }
+}
