ce6cf8e9c3a4274d35484c1e5f95dfd99e700687
marcelovargas <marcelovargas.bo@gmail.com>
2020-05-19 17:53:37 -0300

    Quitar  serviços do token.

--- Livreria/LivreriaWeb/Startup.cs
+++ Livreria/LivreriaWeb/Startup.cs
@@ -79,31 +79,7 @@ namespace LivreriaWeb
 
             #endregion
 
-            services.AddCors();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddControllersWithViews();
-
-            var key = Encoding.ASCII.GetBytes(Settings.Secret);
-
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-             {
-                 x.RequireHttpsMetadata = false;
-                 x.SaveToken = true;
-                 x.TokenValidationParameters = new TokenValidationParameters
-                 {
-                     ValidateIssuerSigningKey = true,
-                     IssuerSigningKey = new SymmetricSecurityKey(key),
-                     ValidateIssuer = false,
-                     ValidateAudience = false
-                 };
-             });
+           
             services.AddRazorPages();
 
             #region Swagger
